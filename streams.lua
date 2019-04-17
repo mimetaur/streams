5e9069c7cd3b8d9dca8be084f1b6f94962c872ff
Nathan Koch <nathanrkoch@gmail.com>
2019-04-17 17:44:52 -0400

    streams controls tuned

--- streams.lua
+++ streams.lua
@@ -11,6 +11,9 @@ local Arcify = include("arcify/lib/arcify")
 local my_arc = arc.connect()
 local arcify = Arcify.new(my_arc, false)
 
+local Billboard = include("billboard/lib/billboard")
+local billboard = Billboard.new()
+
 -- script vars
 local clock = {}
 local spawn_clock = {}
@@ -59,6 +62,7 @@ function init()
             local scaled = util.linlin(0, 5, 1, 15, iv)
             spawn_rate = scaled
             arcify:redraw()
+            billboard:display_param("wind", -1 * value)
         end
     }
 
@@ -69,6 +73,7 @@ function init()
         controlspec = controlspec.new(0, 1, "lin", diffusion_rate, 0.01),
         action = function(value)
             diffusion_rate = value
+            billboard:display_param("diffusion rate", value)
         end
     }
 
@@ -76,13 +81,14 @@ function init()
         type = "control",
         id = "gravity",
         name = "gravity amount",
-        controlspec = controlspec.new(-0.5, 0.5, "lin", 0, 0.01),
+        controlspec = controlspec.new(-0.15, 0.15, "lin", 0, 0.01),
         action = function(value)
-            gravity = value * 0.1
+            gravity = value
+            billboard:display_param("gravity", util.round(gravity, 0.01))
         end
     }
 
-    arcify:register("wind", 0.01)
+    arcify:register("wind", 0.05)
     arcify:register("diffusion", 0.01)
     arcify:register("gravity", 0.01)
     arcify:add_params()
@@ -95,6 +101,7 @@ function redraw()
     screen.clear()
 
     sp:draw()
+    billboard:draw()
 
     screen.update()
 end
